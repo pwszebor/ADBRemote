3d95569a80d92b209d44f66697446468f2ceb18a
pwszebor <pw.wszeborowski@gmail.com>
2021-01-24 12:52:20 +0100

    Initial Commit

---
+++ ADBRemote/ADBRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADBRemoteApp.swift
+//  ADBRemote
+//
+//  Created by Paweł Wszeborowski on 24/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ADBRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADBRemote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ADBRemote
+//
+//  Created by Paweł Wszeborowski on 24/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
